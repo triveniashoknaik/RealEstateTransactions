a017fac0edbe97edc80d61ff1bc3ae9280295e1d
Triveni Ashok Naik <triveniashoknaik21@gmail.com>
2018-07-07 23:49:12 -0700

    Update write.java

--- RealEstateTransactions/src/write.java
+++ RealEstateTransactions/src/write.java
@@ -1,4 +1,28 @@
 
-public class write {
+public class write 
+{
+  public static void calculate(Double sum, Double noofestates, Double answer, String filename)
+	{
+		try
+		{
+			FileWriter fw = new FileWriter(filename,true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			PrintWriter pw = new PrintWriter(bw);
+			
+			pw.println(sum+","+answer+","+noofestates);
+			pw.flush();
+			pw.close();
+			
+			JOptionPane.showMessageDialog(null,"record_saved");
+			
+		} catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null,"record_not_saved");
+		}
+		
+		
+	}
 
 }
